bffa3d4baf2af5ef7087baf9654a6b26e1b4886b
Jonathan Lui <jonathanlui@google.com>
2018-06-26 15:39:08 -0700

    chore: check in synth.py; regenerate v3

---
+++ synth.py
@@ -0,0 +1,32 @@
+import synthtool as s
+import synthtool.gcp as gcp
+import logging
+from pathlib import Path
+import subprocess
+
+logging.basicConfig(level=logging.DEBUG)
+
+gapic = gcp.GAPICGenerator()
+
+versions = ['v3']
+
+for version in versions:
+    library = gapic.node_library(
+        'monitoring', version,
+        config_path="/google/monitoring/"
+                    "artman_monitoring.yaml")
+
+    # skip index, protos, package.json, and README.md
+    s.copy(
+        library,
+        excludes=['package.json', 'README.md', 'src/index.js'],
+    )
+
+#
+# Node.js specific cleanup
+#
+subprocess.run(['npm', 'install'])
+
+# # prettify and lint
+subprocess.run(['npm', 'run', 'prettier'])
+subprocess.run(['npm', 'run', 'lint'])
