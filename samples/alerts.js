e2c1105d33465a50c9bdcaae20f01eea9a96a48b
Syed Ashrafulla <syed@ashraful.la>
2020-12-01 04:00:52 +0000

    enable policies one at a time because the alert policy API does not support concurrent updates to alert policies

--- samples/alerts.js
+++ samples/alerts.js
@@ -254,26 +254,21 @@ async function enablePolicies(projectId, enabled, filter) {
   };
 
   const [policies] = await client.listAlertPolicies(listAlertPoliciesRequest);
-  const tasks = await Promise.all(
-    policies
-      .map(policy => {
-        return {
-          updateMask: {
-            paths: ['enabled'],
-          },
-          alertPolicy: {
-            name: policy.name,
-            enabled: {
-              value: enabled,
-            },
-          },
-        };
-      })
-      .map(updateAlertPolicyRequest =>
-        client.updateAlertPolicy(updateAlertPolicyRequest)
-      )
-  );
-  tasks.forEach(response => {
+  var responses = [];
+  for (const policy of policies) {
+    responses.push(await client.updateAlertPolicy({
+      updateMask: {
+        paths: ['enabled'],
+      },
+      alertPolicy: {
+        name: policy.name,
+        enabled: {
+          value: enabled,
+        },
+      },
+    }));
+  }
+  responses.forEach(response => {
     const alertPolicy = response[0];
     console.log(`${enabled ? 'Enabled' : 'Disabled'} ${alertPolicy.name}.`);
   });
